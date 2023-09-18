cfd5a52e71b95a562e230df9b8378314f3cc78c2
tjm8975 <tjm8975@g.rit.edu>
2023-09-17 20:28:50 -0400

    Updated 4Ups to use Accordian

--- src/4ups.js
+++ src/4ups.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './index.css';
+import { Accordion, AccordionDetails, AccordionSummary } from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { Typography } from '@mui/material';
 
 export default function QuadCharts()
 {
@@ -33,33 +36,61 @@ export default function QuadCharts()
 function QuadChart(props)
 {
     return(
-        <div class="quadChart">
-            <div class="quadDate">
-                <h1>
+        <Accordion>
+            <AccordionSummary
+                expandIcon={<ExpandMoreIcon style={{ color: 'white' }}/>}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+                sx={{
+                    bgcolor: '#1E1E1E',
+                    color: 'white'
+                }}
+            >
+                <Typography
+                    variant="h6"
+                    noWrap
+                    sx={{
+                        mr: 2,
+                        display: { xs: 'none', md: 'flex' },
+                        fontFamily: 'monospace',
+                        fontWeight: 700,
+                        letterSpacing: '.1rem',
+                        color: 'inherit',
+                        textDecoration: 'none',
+                    }}
+                >
                     {props.date}
-                </h1>
-            </div>
-            <div class="row">
-                <div class="column">
-                    <h1>Progress</h1>
-                    <EntryList entries={props.progressList} />
+                </Typography>
+            </AccordionSummary>
+            <AccordionDetails
+                sx={{
+                    bgcolor: '#343434',
+                    color: 'white'
+                }}>
+                <div class="quadChart">
+                    <div class="row">
+                        <div class="column-1">
+                            <h1>Progress</h1>
+                            <EntryList entries={props.progressList} />
+                        </div>
+                        <div class="column-2">
+                            <h1>Risks</h1>
+                            <EntryList entries={props.risksList} />
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="column-3">
+                            <h1>Plans</h1>
+                            <EntryList entries={props.plansList} />
+                        </div>
+                        <div class="column-4">
+                            <h1>Needs</h1>
+                            <EntryList entries={props.needsList} />
+                        </div>
+                    </div>
                 </div>
-                <div class="column">
-                    <h1>Risks</h1>
-                    <EntryList entries={props.risksList} />
-                </div>
-            </div>
-            <div class="row">
-                <div class="column-2">
-                    <h1>Plans</h1>
-                    <EntryList entries={props.plansList} />
-                </div>
-                <div class="column-3">
-                    <h1>Needs</h1>
-                    <EntryList entries={props.needsList} />
-                </div>
-            </div>
-        </div>
+            </AccordionDetails>
+        </Accordion>
     );
 }
 
