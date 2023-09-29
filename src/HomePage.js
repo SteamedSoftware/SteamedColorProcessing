fd0854400a2dd9f398b8c71727bef99a4c873e11
tjm8975 <tjm8975@g.rit.edu>
2023-09-28 21:19:30 -0400

    Update synopsis

--- src/HomePage.js
+++ src/HomePage.js
@@ -8,7 +8,7 @@ function HomePage() {
             <h2>Project Synopsis</h2>
 
             <p>
-                We are developing a tool to be used by digital imaging technicians and cinematographers to allow them to update older LogC3 lookup tables to LogC4 on the spot, visualizing the differences and allowing changes. The program will take in a lookup table using LogC3 along with 1-8 images, the program will run a standard conversion to LogC4, which the user will be able to tweak. The original LogC3 LUT and the produced LogC4 LUT will be displayed by applying them to the input images side-by-side. After review, the user can export the final LogC4 LUT.
+            This project is a tool to be used by digital imaging technicians and cinematographers to allow them to update older LogC3 lookup tables (LUTs) to LogC4 on the spot, visualizing the differences and allowing changes. The program will take in a lookup table using LogC3 along with 1-8 images and run a standard conversion to LogC4, which the user will be able to tweak. This will give creatives the ability to expand the dynamic range and adapt to a new color gamut while preserving the original aesthetic of the lookup tables. The original LogC3 LUT and the produced LogC4 LUT will be displayed by applying them to the input images side-by-side. After review, the user can export the final LogC4 LUT.
             </p>
         </Container>
     );
